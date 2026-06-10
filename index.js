f8e8535255a78757c4024a3a9e2134194fe10cc9
imtiaj70 <imtiajuddinsohag99@gmail.com>
2026-06-10 18:18:41 +0600

    remove in

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(
     origin: [
       process.env.CLIENT_URL,
       "http://localhost:5173",
+      "http://localhost:5174",
       "http://localhost:5000",
       "https://clubsp.netlify.app"
     ],
